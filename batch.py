233852bbed465112f5daaf89a136ba29ce2241e0
Vitali Avagyan <eheva87@gmail.com>
2023-12-10 06:06:39 +0400

    add files

---
+++ batch.py
@@ -0,0 +1,22 @@
+from vllm import LLM, SamplingParams
+import time
+
+prompts = []
+with open('sentences.txt', 'r') as f:
+    prompts = [line.strip() for line in f.readlines()]
+
+max_tokens = 512
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=max_tokens)
+
+llm = LLM(model="TheBloke/Llama-2-70B-AWQ", quantization="AWQ", tensor_parallel_size=2)
+
+start_time = time.time()
+outputs = llm.generate(prompts, sampling_params)
+
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+end_time = time.time()
+print(f"Time taken: {end_time - start_time} seconds")
