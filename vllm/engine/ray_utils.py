f034a9f209b93ca90d1fed841693588b34f148d4
vitali87 <eheva87@gmail.com>
2023-12-20 07:09:16 +0000

    fix: indentation and ray multiple inits

--- vllm/engine/ray_utils.py
+++ vllm/engine/ray_utils.py
@@ -61,18 +61,20 @@ def initialize_cluster(
         distributed backend. `placement_group` includes the specification
         of the resources for each distributed worker.
     """
+    if ray.is_initialized():
+        ray.shutdown()
     if parallel_config.worker_use_ray or engine_use_ray:
         if ray is None:
             raise ImportError(
-                "Ray is not installed. Please install Ray to use distributed serving.")
-        # Only initialize Ray if it's not already initialized
-        if not ray.is_initialized():
-            if is_hip():
-                ray.init(address=ray_address,
-                         ignore_reinit_error=True,
-                         num_gpus=parallel_config.world_size)
-            else:
-                ray.init(address=ray_address, ignore_reinit_error=True)
+                "Ray is not installed. Please install Ray to use distributed "
+                "serving.")
+        # Connect to a ray cluster.
+        if is_hip():
+            ray.init(address=ray_address,
+                    ignore_reinit_error=True,
+                    num_gpus=parallel_config.world_size)
+        else:
+            ray.init(address=ray_address, ignore_reinit_error=True)
 
     if not parallel_config.worker_use_ray:
         # Initialize cluster locally.
